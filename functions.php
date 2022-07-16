fbbbf8046060ab15e7d1210842ae5e87398e2811
yayan fathurohman <107906629+Yayanfr20@users.noreply.github.com>
2022-07-16 07:49:58 +0700

    mengatasi error pada file functions dan delete

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ function create($data) {
 
 function hapus($id) {
     global $conn;
-    mysqli_query($conn, "DELETE FROM albums WHERE id = $id");
+    mysqli_query($conn, "DELETE FROM tabledata WHERE id = $id");
     return mysqli_affected_rows($conn);
 
 }
